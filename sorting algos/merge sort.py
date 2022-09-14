7ef697356628d4a4f04aa74d9c70e1026f4a05ef
DHRITI NIRMAL <62699302+DhritiNirmal@users.noreply.github.com>
2022-09-14 22:37:44 +0530

    Create merge sort.py

---
+++ sorting algos/merge sort.py
@@ -0,0 +1,66 @@
+#Merge sort first divides the array into equal halves and then combines them in a sorted manner.
+
+#divide and conquer algorithm splits a list in half, and keeps splitting the list by 2 until it only has singular elements.
+#Adjacent elements become sorted pairs, then sorted pairs are merged and sorted with other pairs as well. This process continues until we get a sorted list with all the elements of the unsorted input list.
+
+#We recursively split the list in half until we have lists with size one. We then merge each half that was split, sorting them in the process.
+#Sorting is done by comparing the smallest elements of each half. The first element of each list are the first to be compared. 
+#If the first half begins with a smaller value, then we add that to the sorted list. We then compare the second smallest value of the first half with the first smallest value of the second half.
+
+#Every time we select the smaller value at the beginning of a half, we move the index of which item needs to be compared by one.
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # We use the list lengths often, so its handy to make variables
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # We check which value from the start of each list is smaller
+            # If the item at the beginning of the left list is smaller, add it
+            # to the sorted list
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # If the item at the beginning of the right list is smaller, add it
+            # to the sorted list
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # If we've reached the end of the of the left list, add the elements
+        # from the right list
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # If we've reached the end of the of the right list, add the elements
+        # from the left list
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    # If the list is a single element, return it
+    if len(nums) <= 1:
+        return nums
+
+    # Use floor division to get midpoint, indices must be integers
+    mid = len(nums) // 2
+
+    # Sort and merge each half
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Merge the sorted lists into a new one
+    return merge(left_list, right_list)
+
+
+# Verify it works
+random_list_of_nums = [120, 45, 68, 250, 176]
+random_list_of_nums = merge_sort(random_list_of_nums)
+print(random_list_of_nums)
